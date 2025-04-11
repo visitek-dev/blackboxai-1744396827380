de552928ed3006ef98f76f095e2fbac9677ec4d6
tinh <67452477+visitek-dev@users.noreply.github.com>
2025-04-12 01:59:52 +0700

    Add shoe_store_api/app/models/admin.rb 1744397991367

---
+++ shoe_store_api/app/models/admin.rb
@@ -0,0 +1,10 @@
+class Admin < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, :last_name, presence: true
+end
